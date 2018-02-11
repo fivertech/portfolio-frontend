ab2924c17217ed8821fb4eee5dbe47d82424ab92
pedrogmz <pedrogolo@gmail.com>
2018-02-11 13:08:35 -0400

    Layout basico usando Clarity, un framework de CSS para Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ClarityModule } from '@clr/angular';
 import { AppComponent } from './app.component';
 
+// Clarity tutorial
+import { UiModule } from './ui/ui.module';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule,
+		ClarityModule.forRoot(),
+		UiModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
