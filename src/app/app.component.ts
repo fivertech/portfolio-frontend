cc6d30ac505bff30fb6b613288892d6490f8b1a9
pedrogmz <pedrogolo@gmail.com>
2018-02-23 15:55:00 -0400

    Uso de bootstrap como framework de css y quitado Clarity. Creacion de paginas de home, contacto. Implementacion de rutas. Estilos y animaciones.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from "@angular/router";
+import { NgwWowService } from "ngx-wow";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'app';
+  title = "Fiver.tech";
+
+  constructor(private router: Router, private wowService: NgwWowService) {
+    this.router.events
+      .filter(event => event instanceof NavigationEnd)
+      .subscribe(event => {
+        // Reload WoW animations when done navigating to page,
+        // but you are free to call it whenever/wherever you like
+        this.wowService.init();
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,52 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { ClarityModule } from '@clr/angular';
 import { AppComponent } from './app.component';
 
-// Clarity tutorial
-import { UiModule } from './ui/ui.module';
+// Bootstrap modules
+import { AlertModule } from 'ngx-bootstrap';
+
+// Pages components
+import { AppNavbarComponent } from "./app-navbar/app-navbar.component";
+import { AppFooterComponent } from "./app-footer/app-footer.component";
+import { InicioComponent } from "./inicio/inicio.component";
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+import { PortafolioComponent } from "./portafolio/portafolio.component";
+import { ContactoComponent } from "./contacto/contacto.component";
+
+// Animation module
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgwWowModule } from "ngx-wow";
+
+// Rutas
+import { AppRoutingModule } from ".//app-routing.module";
+import { RouterModule, Routes } from "@angular/router";
+
+const routes: Routes = [
+  { path: "contacto", component: ContactoComponent },
+  { path: "inicio", component: InicioComponent },
+  { path: "", redirectTo: "/inicio", pathMatch: "full" },
+  { path: "**", component: PageNotFoundComponent }
+];
 
 @NgModule({
-	declarations: [
-		AppComponent
-	],
-	imports: [
-		BrowserModule,
-		ClarityModule.forRoot(),
-		UiModule
-	],
-	providers: [],
-	bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    AppNavbarComponent,
+    AppFooterComponent,
+    ContactoComponent,
+    InicioComponent,
+    PageNotFoundComponent,
+    PortafolioComponent
+  ],
+  imports: [
+    BrowserModule,
+    AlertModule.forRoot(),
+    NgwWowModule.forRoot(),
+    RouterModule.forRoot(routes),
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
